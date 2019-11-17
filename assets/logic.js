e93e7880fc269d239d697eafd61d46ac9a0bb0d1
Andrew Weihe <andrewy03@gmail.com>
2019-11-16 23:09:39 -0600

    cleaning it up

--- assets/logic.js
+++ assets/logic.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", function (event) {  //waits for page load
-    //DOM targeting elements
+document.addEventListener("DOMContentLoaded", function (event) {  //waits for page to load
+    //targeting DOM elements
     var startBtn = document.getElementById("startButton");
 
     var initCard = document.getElementById("initalCard");
@@ -14,26 +14,26 @@ document.addEventListener("DOMContentLoaded", function (event) {  //waits for pa
 
 
     var questText = document.getElementById("questionPrompt");
-    // var answerBtns = document.getElementById("answerArea");
+    var answerBtns = document.getElementById("answerArea");
     var clearBtn = document.getElementById("clearBtn");
     var clearBtnArea = document.getElementById("clearBtnArea");
 
-    //Question Answers
+    //Answer texts
     var a = document.getElementById("AnswerA");
     var b = document.getElementById("AnswerB");
-    var c = document.getElementById("AnswerC");
+    var c = document.getElementById("AnswerC");   
     var d = document.getElementById("AnswerD");
     //Answer buttons
-    var BtnA = document.getElementById("AnswerBtnA");
-    var BtnB = document.getElementById("AnswerBtnB");
-    var BtnC = document.getElementById("AnswerBtnC");
-    var BtnD = document.getElementById("AnswerBtnD");
+    var aBtn = document.getElementById("AnswerBtnA");
+    var bBtn = document.getElementById("AnswerBtnB");
+    var cBtn = document.getElementById("AnswerBtnC");
+    var dBtn = document.getElementById("AnswerBtnD");
 
-    //variable stack - noramlly wouldn't use globals but it's a small application (and I'm novice)
-    var problemTime = 15;    //time per question and points factor
-    var penalty = 5;        //time penalty for a wrong answer
-    //Modify the above to change the quiz dynamics
-    
+    //time per question and points factor
+    var problemTime = 15;    
+    //time penalty for a wrong answer
+    var penalty = 5; 
+
     var iter = 0;
     var timer = 75;
     var endTime = 0;
@@ -45,3 +45,25 @@ document.addEventListener("DOMContentLoaded", function (event) {  //waits for pa
     var scoreList = [];
     var maxScore = 0;
     var scoreShown = false;
+
+    //quiz info on how scores are calc
+    instructions.textContent = ("Your score is the number of correct answers times "+problemTime+", plus the time left. Be careful - wrong answers will also subtract "+penalty+" seconds from the time remaining!");
+
+    //Randomize array element order in-place.
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        return array;
+    }
